77282058458eafb432c7fcf96991f5ae0f1f7597
BrianFZ1511 <236w0492@zongolica.tecnm.mx>
2026-03-19 21:08:27 -0600

    Secuence eliminado (19/03/2026)

--- src/main/java/Logica/Usuario.java
+++ src/main/java/Logica/Usuario.java
@@ -16,7 +16,7 @@ import javax.persistence.Table;
 public class Usuario implements Serializable {
     
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String nombre;
     private String apellidos;
