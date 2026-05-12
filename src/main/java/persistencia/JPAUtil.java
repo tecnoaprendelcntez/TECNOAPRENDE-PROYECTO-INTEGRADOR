0c1b737202b1696c4bd3d74383a25ce06980cb48
tecnoaprendelcntez <tecnoaprende.lcntez@gmail.com>
2026-05-12 12:36:44 -0600

    Modificacion de JPAUtil (12/05/2026)

--- src/main/java/persistencia/JPAUtil.java
+++ src/main/java/persistencia/JPAUtil.java
@@ -16,24 +16,39 @@ public class JPAUtil {
 
             String mysqlUrl = System.getenv("MYSQL_URL");
 
-            if (mysqlUrl != null) {
-                //Railway
+            if (mysqlUrl != null && !mysqlUrl.isEmpty()) {
+                // Railway
 
                 String user = System.getenv("MYSQLUSER");
                 String password = System.getenv("MYSQLPASSWORD");
 
-                String url = mysqlUrl.replace("mysql://", "jdbc:mysql://")
-                        + "?useSSL=false&serverTimezone=UTC&useUnicode=true&characterEncoding=UTF-8";
+                // Si la URL viene como mysql:// la convertimos a jdbc:mysql://
+                if (mysqlUrl.startsWith("mysql://")) {
+                    mysqlUrl = mysqlUrl.replaceFirst("mysql://", "jdbc:mysql://");
+                }
+
+                // Agregar parámetros necesarios
+                String separator = mysqlUrl.contains("?") ? "&" : "?";
+                String url = mysqlUrl + separator
+                        + "useSSL=false"
+                        + "&allowPublicKeyRetrieval=true"
+                        + "&serverTimezone=UTC"
+                        + "&useUnicode=true"
+                        + "&characterEncoding=UTF-8";
 
                 props.put("javax.persistence.jdbc.url", url);
                 props.put("javax.persistence.jdbc.user", user);
                 props.put("javax.persistence.jdbc.password", password);
 
             } else {
-                //Local
+                // Local (XAMPP)
 
                 props.put("javax.persistence.jdbc.url",
-                        "jdbc:mysql://localhost:3306/dbtecnoaprende?serverTimezone=America/Mexico_City&useUnicode=true&characterEncoding=UTF-8");
+                        "jdbc:mysql://localhost:3306/dbtecnoaprende"
+                        + "?serverTimezone=America/Mexico_City"
+                        + "&useUnicode=true"
+                        + "&characterEncoding=UTF-8");
+
                 props.put("javax.persistence.jdbc.user", "Admin1");
                 props.put("javax.persistence.jdbc.password", "mdcgr050504");
             }
